8ec06c25f4d2e04087f8ae1d910c0d89877a3b8d
Pavlova Marina <marizza.mil89@gmail.com>
2021-11-28 00:03:55 +0200

    SQLite3 DataBase Tutorial

--- main/views.py
+++ main/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
 
-def index(request):
-    return HttpResponse("Tech with Itay!")
+def index(request, name):
+    ls = ToDoList.objects.get(name=name)
+
+    item = ls.item_set.get(id=1)
+
+    return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" % (ls.name, item.text))
+
 
