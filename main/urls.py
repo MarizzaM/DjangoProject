9a731b5a602849da73862b68c8bb03be5462ea5d
Pavlova Marina <marizza.mil89@gmail.com>
2021-12-04 22:34:40 +0200

    User Specific Pages/Access

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('<int:id>', views.index, name='index'),
     path("", views.home, name="home"),
     path("home/", views.home, name="home"),
-    path("create/", views.create, name="index")
+    path("create/", views.create, name="index"),
+    path("view/", views.view, name="view"),  # <-- added
 
 ]
--- main/views.py
+++ main/views.py
@@ -8,25 +8,29 @@ from .forms import CreateNewList
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
 
-    if response.method == "POST":
-        if response.POST.get("save"):
-            for item in ls.item_set.all():
-                if response.POST.get("c" + str(item.id)) == "clicked":
-                    item.complete = True
+    if ls in response.user.todolist.all():
+
+        if response.method == "POST":
+            if response.POST.get("save"):
+                for item in ls.item_set.all():
+                    if response.POST.get("c" + str(item.id)) == "clicked":
+                        item.complete = True
+                    else:
+                        item.complete = False
+
+                    item.save()
+
+            elif response.POST.get("newItem"):
+                txt = response.POST.get("new")
+
+                if len(txt) > 2:
+                    ls.item_set.create(text=txt, complete=False)
                 else:
-                    item.complete = False
+                    print("invalid")
 
-                item.save()
+        return render(response, "main/list.html", {"ls": ls})
 
-        elif response.POST.get("newItem"):
-            txt = response.POST.get("new")
-
-            if len(txt) > 2:
-                ls.item_set.create(text=txt, complete=False)
-            else:
-                print("invalid")
-
-    return render(response, "main/list.html", {"ls": ls})
+    return render(response, "main/home.html", {})
 
 
 def home(response):
@@ -46,6 +50,7 @@ def create(response):
             n = form.cleaned_data["name"]
             t = ToDoList(name=n)
             t.save()
+            response.user.todolist.add(t)  # adds the to do list to the current logged in user
 
         return HttpResponseRedirect("/%i" % t.id)
 
@@ -53,3 +58,7 @@ def create(response):
         form = CreateNewList()
 
     return render(response, "main/create.html", {"form": form})
+
+
+def view(response):
+    return render(response, "main/view.html", {})
